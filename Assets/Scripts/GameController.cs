c15659baa63f1d0e4d8817638613604188337ffe
caneslotocato <caneslotocato1@gmail.com>
2016-01-27 01:07:07 +0100

    Stefano
    Cose

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -7,10 +7,19 @@ public class GameController : MonoBehaviour
     public GameObject[] playerTrace = new GameObject[2];
     public GameObject enemy ;
     private int index = 0;
-    
+    public float MinimunXAxys;
+    public float MaxXAxys;
+    public float MinYAxys;
+    public float MaxYAxys;
+    private bool IsSpawned;
+    public int MaxNumberOfAnimals;
+    public GameObject animalPowerUp;
+    public GameObject[] Animals;
+    private GameObject[] AnimalsInScene;
 
-	// Use this for initialization
-	void Start ()
+
+    // Use this for initialization
+    void Start ()
     {
         
 	}
@@ -18,7 +27,7 @@ public class GameController : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
     {
-	    
+        InstantiateEnemy();
 	}
 
     public void playerAdd(GameObject playerGameobject)
@@ -38,5 +47,24 @@ public class GameController : MonoBehaviour
             enemy = playerTrace[0];
         }
         return enemy;
+    }
+    public void InstantiateEnemy()
+    {
+        if (IsSpawned == false)
+        {
+            for (int i = 0; i < MaxNumberOfAnimals; i++)
+            {
+                GameObject obj;
+                int AnimalToSpawn = Random.Range(0, 1);
+
+                Vector3 PositionToSpawn = new Vector3(Random.Range(MinimunXAxys, MaxXAxys), 0, Random.Range(MinYAxys, MaxYAxys));
+                obj = Instantiate(Animals[AnimalToSpawn], PositionToSpawn, transform.rotation) as GameObject;
+                AnimalsInScene[i] = obj;
+                if (i < 5)
+                {
+                    IsSpawned = true;
+                }
+            }
+        }
     }
 }
