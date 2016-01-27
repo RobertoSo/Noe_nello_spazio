0d3c9eab10caefcf90335d0582e5a1dfd0c98168
RobertoSo <soatto.ro@gmail.com>
2016-01-27 11:02:50 +0100

    ROberto

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour
+{
+    
+    public GameObject[] playerTrace = new GameObject[2];
+    public GameObject enemy ;
+    private int index = 0;
+    public float MinimunXAxys;
+    public float MaxXAxys;
+    public float MinYAxys;
+    public float MaxYAxys;
+    private bool IsSpawned;
+    public int MaxNumberOfAnimals;
+    public GameObject animalPowerUp;
+    public GameObject[] Animals;
+    private GameObject[] AnimalsInScene;
+
+
+
+    // Use this for initialization
+    void Start ()
+    {
+        
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        InstantiateEnemy();
+    }
+	
+
+
+    public void playerAdd(GameObject playerGameobject)
+    {
+        playerTrace[index] = playerGameobject;
+        index++;
+    }
+
+    public void InstantiateEnemy()
+    {
+        if (IsSpawned == false)
+        {
+            for (int i = 0; i < MaxNumberOfAnimals; i++)
+            {
+                GameObject obj;
+                int AnimalToSpawn = Random.Range(0, 1);
+
+                Vector3 PositionToSpawn = new Vector3(Random.Range(MinimunXAxys, MaxXAxys), 0, Random.Range(MinYAxys, MaxYAxys));
+                obj = Instantiate(Animals[AnimalToSpawn], PositionToSpawn, transform.rotation) as GameObject;
+                AnimalsInScene[i] = obj;
+                if (i < 5)
+                {
+                    IsSpawned = true;
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,146 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour 
+{
+   
+    
+    public float movementSpeed;
+    public float TimeToRotate;
+    public Weapon weapon;
+    public GameController gameController;
+    public GameObject enemyGameobject;
+    private int myWeapon;
+
+    void Awake()
+    {
+        
+    }
+	// Use this for initialization
+	void Start ()
+    {
+        gameController = Camera.main.GetComponent<GameController>();
+        gameController.playerAdd(this.gameObject);
+        weapon = this.gameObject.GetComponent<Weapon>();
+        WhoIsEnemy(this.gameObject);
+        //enemyGameobject =  gameController.GetEnemy(this.gameObject);
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        FirstShipMovement();
+        SecondShipMovement();
+        PlayerOneShoot();
+        PlayerTwoShoot();
+        
+	}
+
+    private void FirstShipMovement()
+    {
+        if (this.gameObject.CompareTag("PlayerOne"))
+        {
+            if (Input.GetKey(KeyCode.W))
+            {
+                Quaternion rotationUp = Quaternion.Euler(0, 90, 0);
+                transform.rotation = Quaternion.Lerp(transform.rotation, rotationUp, TimeToRotate * Time.time);
+                transform.Translate(Vector3.forward * movementSpeed * Time.deltaTime);
+            }
+            if (Input.GetKey(KeyCode.S))
+            {
+                Quaternion rotationDown = Quaternion.Euler(0, -90, 0);
+                transform.rotation = Quaternion.Lerp(transform.rotation, rotationDown, TimeToRotate * Time.time);
+                transform.Translate(Vector3.forward * movementSpeed * Time.deltaTime);
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                Quaternion rotationLeft = Quaternion.Euler(0, 180, 0);
+                transform.rotation = Quaternion.Lerp(transform.rotation, rotationLeft, TimeToRotate * Time.time);
+                transform.Translate(Vector3.forward * movementSpeed * Time.deltaTime);
+            }
+            if (Input.GetKey(KeyCode.A))
+            {
+                Quaternion rotationRight = Quaternion.Euler(0, 0, 0);
+                transform.rotation = Quaternion.Lerp(transform.rotation, rotationRight, TimeToRotate * Time.time);
+                transform.Translate(Vector3.forward * movementSpeed * Time.deltaTime);
+            }
+        }
+       
+    }
+    private void SecondShipMovement()
+    {
+        if (this.gameObject.CompareTag("PlayerTwo"))
+        {
+            if (Input.GetKey(KeyCode.UpArrow))
+            {
+                Quaternion rotationUp = Quaternion.Euler(0, 90, 0);
+                transform.rotation = Quaternion.Lerp(transform.rotation, rotationUp, TimeToRotate * Time.time);
+                transform.Translate(Vector3.forward * movementSpeed * Time.deltaTime);
+            }
+            if (Input.GetKey(KeyCode.DownArrow))
+            {
+                Quaternion rotationDown = Quaternion.Euler(0, -90, 0);
+                transform.rotation = Quaternion.Lerp(transform.rotation, rotationDown, TimeToRotate * Time.time);
+                transform.Translate(Vector3.forward * movementSpeed * Time.deltaTime);
+            }
+            if (Input.GetKey(KeyCode.RightArrow))
+            {
+                Quaternion rotationLeft = Quaternion.Euler(0, 180, 0);
+                transform.rotation = Quaternion.Lerp(transform.rotation, rotationLeft, TimeToRotate * Time.time);
+                transform.Translate(Vector3.forward * movementSpeed * Time.deltaTime);
+            }
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                Quaternion rotationRight = Quaternion.Euler(0, 0, 0);
+                transform.rotation = Quaternion.Lerp(transform.rotation, rotationRight, TimeToRotate * Time.time);
+                transform.Translate(Vector3.forward * movementSpeed * Time.deltaTime);
+            }
+        }
+    }
+
+    private void PlayerOneShoot()
+    {
+        if(this.gameObject.CompareTag("PlayerOne"))
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                /* Debug.Log(weapon);
+                 Debug.Log(gameController);
+                 Debug.Log(enemyGameobject);
+                 weapon.bullWeapon(weapon.projectileSpawnPoint[3], enemyGameobject.transform);*/
+                weapon.hedgehogWeapon();
+            }
+        }
+        
+    }
+
+    private void PlayerTwoShoot()
+    {
+        if (this.gameObject.CompareTag("PlayerTwo"))
+        {
+            if (Input.GetKeyDown(KeyCode.KeypadEnter))
+            {
+                weapon.bullWeapon(weapon.projectileSpawnPoint[3], enemyGameobject.transform);
+            }
+        }
+    }
+
+    
+
+    private void WhoIsEnemy(GameObject obj)
+    {
+        if (obj.CompareTag("PlayerOne"))
+        {
+            enemyGameobject = GameObject.FindGameObjectWithTag("PlayerTwo");
+        }
+        else
+        {
+            enemyGameobject = GameObject.FindGameObjectWithTag("PlayerOne");
+        }
+    }
+
+   /* private void MyWeapon()
+    {
+        if(Input.GetKeyDown(KeyCode.))
+    }*/
+}
