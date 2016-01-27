fbac75a2a6f6260d816f3ca3ebaedce0b9a9c7bf
RobertoSo <soatto.ro@gmail.com>
2016-01-27 14:41:52 +0100

    Roberto
    Cose a caso

---
+++ Assets/Scripts/RotazionePianeta.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotazionePianeta: MonoBehaviour {
+
+	public float rotationSpeed;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		PlanetRotation ();
+
+
+	}
+
+	void PlanetRotation() {
+//		transform.RotateAround(transform.position, Vector3.up , rotationSpeed); 
+		transform.Rotate(0, rotationSpeed, 0, Space.Self);
+	}
+}
